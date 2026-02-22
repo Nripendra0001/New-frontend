4a8cdfd9663f2676d63a35c96471bfb036a35e95
Nirpendra Kumar Pandey <nirpendrapandey13@gmail.com>
2026-02-22 12:23:19 +0530

    fix deploy script

--- deploy.bat
+++ deploy.bat
@@ -1,14 +1,14 @@
 @echo off 
 title SarkariNext Git Auto Deploy 
-cd /d "%%~dp0" 
+cd /d %C:\Users\NIRPENDRA PANDEY\Desktop\Sarkari Bharat\SarkariNext\New-frontend-main% 
 echo [1/5] Git Status... 
 git status 
 echo [2/5] Adding files... 
 git add . 
 set /p msg=Enter commit message (default: update): 
-if "%%msg%%"=="" set msg=update 
+if "%del deploy.bat%"=="" set msg=update 
 echo [3/5] Commit... 
-git commit -m "%%msg%%" 
+git commit -m "%del deploy.bat%" 
 echo [4/5] Push... 
 git push origin main 
 echo Done ?? 
